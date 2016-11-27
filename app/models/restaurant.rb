921bda696172f28f555c92d7dc5469cad20611ef
Raghu Betina <raghu@firstdraft.com>
2016-11-27 14:33:33 +0000

    Direct association between restaurants and staffs

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,10 @@
 class Restaurant < ApplicationRecord
   # Direct associations
 
+  has_many   :staffs,
+             :foreign_key => "home_restaurant_id",
+             :dependent => :nullify
+
   belongs_to :user
 
   # Indirect associations
--- app/models/staff.rb
+++ app/models/staff.rb
@@ -1,6 +1,9 @@
 class Staff < ApplicationRecord
   # Direct associations
 
+  belongs_to :home_restaurant,
+             :class_name => "Restaurant"
+
   belongs_to :user
 
   # Indirect associations
