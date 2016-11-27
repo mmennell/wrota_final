b28decba5b6a09c53705a05aa93a5ac32927973a
Raghu Betina <raghu@firstdraft.com>
2016-11-27 14:33:56 +0000

    Whitelist columns in Role for ActiveAdmin

--- app/admin/role.rb
+++ app/admin/role.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Role do
 
+ permit_params :role_name, :description, :restaurant_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
